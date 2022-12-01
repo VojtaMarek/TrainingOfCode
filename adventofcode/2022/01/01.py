316f92ac1badde73daa5240c3c9482751182a4b1
VojtaMarek <vojtama@gmail.com>
2022-12-01 14:31:20 +0100

    initial commit

---
+++ adventofcode/2022/01/01.py
@@ -0,0 +1,18 @@
+def get_sums():
+    """returns 2D structured lines"""
+    with open('AdventOfCode\\2022\\01\\input.txt', 'rt') as f:
+        lines = f.read().split('\n\n')
+        for i, line in enumerate(lines):
+            lines[i] = sum(map(int, line.split('\n')))
+        return lines
+
+def main():
+    sums = get_sums()
+    sums = sorted(sums, reverse=True)
+    
+    print('Part one:', sums[0])
+    print('Part two:', sum(sums[0:3]))
+
+
+if __name__ == '__main__':
+    main()
