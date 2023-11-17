da7dee0cffcfd0cb32ee496bcb02a3d042bcca17
Vojtěch Marek <vojtama@gmail.com>
2023-11-17 21:14:13 +0100

    cross platform compatibility with os.path.join(), refactoring

--- make_dirs_for_AoC.py
+++ make_dirs_for_AoC.py
@@ -1,20 +1,32 @@
-"""This code check if you are ready for Advent of Code. In case, you have no day folders (eg. 01, 02, ..)
-in current year folder (eg. 2022/) created, it gives you an option to do so."""
+"""
+This script check if you are ready for Advent of Code. In case, you have no day folders (eg. 01, 02, ..)
+in current year folder (eg. 2022/) created, it gives you an option to do so.
+
+1.  Clone this script in folder where you like to have the empty files created in structure eg. ./2022/01/01.py
+2.  Modify the constant variables such as YEAR, PATH, and FILE_TYPE
+3.  Run the script and follow the instructions in your terminal.
+"""
 
 import os
 import datetime
 
-YEAR = '2016' # datetime.datetime.today().strftime('%Y') # un-comment for current year
+
+YEAR = datetime.datetime.today().strftime('%Y')
 PATH = 'advent_of_code' # os.path.abspath(os.getcwd())
 FILE_TYPE = '.py'
 
 
 def get_path_with_year(current_year=YEAR, current_path=PATH):
-    year_path = f'{current_path}\\{current_year}'
+    year_path = os.path.join(current_path, current_year)
     return year_path
 
 
-def check_folder(path=get_path_with_year(), create_folders=False, create_input_files=False, create_code_files=False, create_input_test_files=False):
+def check_folder(path=get_path_with_year(),
+                 create_folders=False,
+                 create_input_files=False,
+                 create_code_files=False,
+                 create_input_test_files=False
+                 ):
     flag = True
     new_folders = []
     for folder in range(1, 26):
@@ -24,7 +36,11 @@ def check_folder(path=get_path_with_year(), create_folders=False, create_input_f
             new_folders.append(str(folder))
 
     for new_folder in new_folders:
-        new_path = f'{path}\\{new_folder}\\'
+        new_path = os.path.join(path, new_folder)
+        new_path_input = os.path.join(new_path, 'input.txt')
+        new_path_input_test = os.path.join(new_path, 'input.txt')
+        new_path_code = os.path.join(new_path, new_folder + FILE_TYPE)
+
         if not os.path.exists(new_path):
             print(
                 "Folder does not exist: "
@@ -41,23 +57,22 @@ def check_folder(path=get_path_with_year(), create_folders=False, create_input_f
             print("Folder exists: " + new_path + " -> no need to make a new one.")
         if create_input_files:
             try:
-                with open(f"{new_path}input.txt", "x") as input_file: input_file.write('')
-                print(f"{new_path}input.txt -> WAS CREATED.")
+                with open(new_path_input, "x") as input_file: input_file.write('')
+                print(f"{new_path_input} -> WAS CREATED.")
             except:
-                print(f"{new_path}input.txt -> WAS NOT CREATED.")
+                print(f"{new_path_input} -> WAS NOT CREATED.")
         if create_code_files:
             try:
-                with open(f"{new_path}{new_folder}{FILE_TYPE}", "x") as code_file: code_file.write('')
-                print(f"{new_path}{new_folder}{FILE_TYPE} -> WAS CREATED.")
+                with open(new_path_code, "x") as code_file: code_file.write('')
+                print(f"{new_path_code} -> WAS CREATED.")
             except:
-                print(f"{new_path}{new_folder}{FILE_TYPE} -> WAS NOT CREATED.")
+                print(f"{new_path_code} -> WAS NOT CREATED.")
         if create_input_test_files:
             try:
-                with open(f"{new_path}input_test.txt", "x") as input_test_file: input_test_file.write('')
-                print(f"{new_path}input_test.txt -> WAS CREATED.")
+                with open(new_path_input_test, "x") as input_test_file: input_test_file.write('')
+                print(f"{new_path_input_test} -> WAS CREATED.")
             except:
-                print(f"{new_path}input_test.txt -> WAS NOT CREATED.")
-
+                print(f"{new_path_input_test} -> WAS NOT CREATED.")
 
     return flag
 
